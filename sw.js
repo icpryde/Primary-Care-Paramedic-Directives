61405ced6349194f6b6f0f7c8cea9bdf7e0965da
Chris <kurisu@macbookpro.lan>
2026-03-23 20:18:14 -0400

    Revert "Fix swipe transition geometry regressions and clipping"
    This reverts commit 842f02646eb2bbafe18eab9e799d507b104ab9ac.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pcp-directives-v60';
+const CACHE_NAME = 'pcp-directives-v59';
 const BASE = (self.location && self.location.hostname === 'icpryde.github.io')
   ? '/Primary-Care-Paramedic-Directives'
   : '';
