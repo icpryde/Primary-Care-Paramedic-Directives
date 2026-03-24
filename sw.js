9070fc0a3a9828f67a1fd1db601fa271dbc7bc3b
Chris <kurisu@macbookpro.lan>
2026-03-23 20:18:14 -0400

    Revert "Fix swipe blue flash: preserve view backgrounds during transitions"
    This reverts commit be1c57978f88a15631cb50a7161f760e71f8383d.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pcp-directives-v59';
+const CACHE_NAME = 'pcp-directives-v58';
 const BASE = (self.location && self.location.hostname === 'icpryde.github.io')
   ? '/Primary-Care-Paramedic-Directives'
   : '';
